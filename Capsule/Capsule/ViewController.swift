83f9af93640e7bc09a8806b08ef3194fbb08d427
Gio <gio@Gios-MBP.home>
2020-10-20 23:01:55 -0400

    initial commit

---
+++ Capsule/Capsule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capsule
+//
+//  Created by Gio Lomsa on 10/20/20.
+//  Copyright © 2020 Giorgi Lomsadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
